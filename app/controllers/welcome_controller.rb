1e1323e5416840a28d7a13ea7451be6c66b89e2c
dongkibravo <dongpark@Dongs-MacBook-Pro.local>
2017-02-14 21:02:27 +0900

    asdf

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 require 'fcm'
 class WelcomeController < ApplicationController
+
   def index
-  	protect_from_forgery with: :exception
   	fcm = FCM.new("AIzaSyA5gF4_bnQFVCilP3IDjUKKoWlIvAVf6o8")
 
   	registration_ids= ["doikMFq2WfU:APA91bEcP3KXDX27DWJXh-e03wJUdSW-yudeqAoy9u1NbRbZklPm43rZrAQZsLUwBM6-Fp3_-0428rpNyxCfea6mp5gd9Q3S4Py2AVhetRShoWBE3AD9kMOuxCcxQDQZx6QUptI3YUc-"] # an array of one or more client registration tokens
